9e6b004064d783d694a1799810805edf956fe301
Ethan Mertz <ethanmertz@cs.uchicago.edu>
2018-01-29 14:53:09 -0600

    just added some whitespace and a comment

--- map/concerts/views.py
+++ map/concerts/views.py
@@ -19,10 +19,12 @@ CACHE_PATH = ".cache-info"
 SP_OAUTH = oauth2.SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, scope=SCOPE, cache_path=CACHE_PATH)
 
 def index(request):
+    
     authorize_url = SP_OAUTH.get_authorize_url()
     return render(request, 'concerts/index.html', {'authorize_url': authorize_url})
 
 def map(request):
+
     token_info = SP_OAUTH.get_cached_token()
 
     if not token_info:
@@ -41,22 +43,42 @@ def map(request):
     return render(request, 'concerts/map.html', {'data': data})
 
 def get_data(artist):
+    '''
+    Given an artists name, returns a list of lists, each containing the address of the venue where the artist is performing
+    and a html tag containg the ticket information and time of the show.
+
+    Inputs: 
+        artist (str): the name of the artist
+
+    Returns: list of lists 
+    '''
+
     response_1 = url.urlopen('https://www.songkick.com/search?utf8=%E2%9C%93&type=initial&query={}'.format(url.quote(artist)))
     html_1 = response_1.read()
     soup_1 = bs4.BeautifulSoup(html_1, 'html.parser')
+
     is_artist = soup_1.find('li', class_='artist')
+
     if is_artist:
+
         artist_url = 'https://www.songkick.com' + is_artist.find('a', class_='thumb')['href']
+
         response_2 = url.urlopen(artist_url)
         html_2 = response_2.read()
         soup_2 = bs4.BeautifulSoup(html_2, 'html.parser')
+
         on_tour = soup_2.find('li', class_='ontour').get_text()
+
         if on_tour == 'On tour: yes':
+
             concerts_url = artist_url + '/calendar'
+
             response_3 = url.urlopen(concerts_url)
             html_3 = response_3.read()
             soup_3 = bs4.BeautifulSoup(html_3, 'html.parser')
+
             times = [tag.get_text(strip=True) for tag in soup_3.find_all('li', class_="with-date")]
             tickets = ['https://www.songkick.com' + tag.parent['href'] for tag in soup_3.find_all('span', class_='button buy-tickets')]
             addresses = [' '.join(tag.next_sibling.next_sibling.stripped_strings) for tag in soup_3.find_all('span', class_="venue-name")]
+
     return [[address, "<a target=_blank href='{}'>{}</a>".format(ticket, time)] for address, ticket, time in zip(addresses, tickets, times)]
